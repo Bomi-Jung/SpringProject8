e403913376fbc2f87f2d360addce239c854991a2
Jungbomi <bomi7586@gmail.com>
2024-01-16 13:45:46 +0900

    init

--- src/main/java/com/example/demo/service/BoardServiceImpl.java
+++ src/main/java/com/example/demo/service/BoardServiceImpl.java
@@ -1,5 +1,9 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +20,6 @@ public class BoardServiceImpl implements BoardService {
 	@Override
 	public int register(BoardDTO dto) {
 
-		System.out.println(dto);
 
 		Board entity = dtoToEntity(dto);
 
@@ -27,5 +30,20 @@ public class BoardServiceImpl implements BoardService {
 		System.out.println(dto);
 
 		return newNo;
+	}
+
+	@Override
+	public List<BoardDTO> getList() {
+		
+		//데이터 베이스에서 게시물 목록을 가져오기
+		List <Board> result = repository.findAll();
+		//리스트 샹송
+		List <BoardDTO> list = new ArrayList<>();
+		
+		list = result.stream()//리스트에서 스트림 생성 
+				.map(entity -> entityToDto(entity))// 중산연산으로 엔티티를 dto로 변환
+				.collect(Collectors.toList()); // 
+		
+		return list;
 	}
 }
--- src/test/java/com/example/demo/service/BoardServiceTest.java
+++ src/test/java/com/example/demo/service/BoardServiceTest.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +23,15 @@ public class BoardServiceTest {
 		
 		System.out.println("새로운 게시물 번호: " + no);
 		
+	}
+	
+	@Test
+	public void 게시물목록조회() {
+		List<BoardDTO> list = service.getList();
+		
+		for(BoardDTO dto : list) {
+			System.out.println(dto);
+		}
 	}
 
 }
